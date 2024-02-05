a3074a04a7e12ca78cd9612700c39eafec73c0cc
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-02-04 21:59:28 -0600

    The end

---
+++ my_module.py
@@ -0,0 +1,8 @@
+### Módulo para pruebas ###
+#Los modulos no pueden comenzar por numeros o caracteres especiales
+def sumValue(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+
+
+def printValue(value):
+    print(value)
